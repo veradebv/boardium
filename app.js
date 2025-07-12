41514120c678d2417545e974e5bdc8fa007d34cc
veradebv <veradebora@outlook.co.id>
2025-07-12 15:16:17 +0700

    protect routes using JWT

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const authMiddleware = require('./middleware/authMiddleware');
 
 dotenv.config();
-connectDB();
 
 const app = express();
 app.use(cors());
@@ -14,8 +14,17 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));   
 app.use('/api/boards',require('./routes/boardRoutes'));
 
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
     res.send('Boardium API is running 🚀');
+});
+
+app.get('/api/secret', authMiddleware, (req, res) => {
+    res.json({
+        message: 'You are authenticated ✅',
+        userId: req.user 
+    });
 });
 
 const PORT = process.env.PORT || 8080;
